1fc85835d1cbd038e7a255e8a6b6793cc3defa89
maxwell1999 <maxwell@buffer.finance>
2025-09-15 20:42:29 +0530

    algotrade:0.0.6

--- scripts/v2_with_controllers.py
+++ scripts/v2_with_controllers.py
@@ -9,7 +9,10 @@ from hummingbot.connector.connector_base import ConnectorBase
 from hummingbot.data_feed.candles_feed.data_types import CandlesConfig
 from hummingbot.strategy.strategy_v2_base import StrategyV2Base, StrategyV2ConfigBase
 from hummingbot.strategy_v2.models.base import RunnableStatus
-from hummingbot.strategy_v2.models.executor_actions import CreateExecutorAction, StopExecutorAction
+from hummingbot.strategy_v2.models.executor_actions import (
+    CreateExecutorAction,
+    StopExecutorAction,
+)
 
 
 class V2WithControllersConfig(StrategyV2ConfigBase):
@@ -31,9 +34,12 @@ class V2WithControllers(StrategyV2Base):
     specific controller and wait until the active executors finalize their execution. The rest of the executors will
     wait until the main strategy stops them.
     """
+
     performance_report_interval: int = 1
 
-    def __init__(self, connectors: Dict[str, ConnectorBase], config: V2WithControllersConfig):
+    def __init__(
+        self, connectors: Dict[str, ConnectorBase], config: V2WithControllersConfig
+    ):
         super().__init__(connectors, config)
         self.config = config
         self.max_pnl_by_controller = {}
@@ -66,19 +72,31 @@ class V2WithControllers(StrategyV2Base):
             else:
                 current_drawdown = last_max_pnl - controller_pnl
                 if current_drawdown > self.config.max_controller_drawdown_quote:
-                    self.logger().info(f"Controller {controller_id} reached max drawdown. Stopping the controller.")
+                    self.logger().info(
+                        f"Controller {controller_id} reached max drawdown. Stopping the controller."
+                    )
                     controller.stop()
                     executors_order_placed = self.filter_executors(
                         executors=self.get_executors_by_controller(controller_id),
                         filter_func=lambda x: x.is_active and not x.is_trading,
                     )
                     self.executor_orchestrator.execute_actions(
-                        actions=[StopExecutorAction(controller_id=controller_id, executor_id=executor.id) for executor in executors_order_placed]
+                        actions=[
+                            StopExecutorAction(
+                                controller_id=controller_id, executor_id=executor.id
+                            )
+                            for executor in executors_order_placed
+                        ]
                     )
                     self.drawdown_exited_controllers.append(controller_id)
 
     def check_max_global_drawdown(self):
-        current_global_pnl = sum([self.get_performance_report(controller_id).global_pnl_quote for controller_id in self.controllers.keys()])
+        current_global_pnl = sum(
+            [
+                self.get_performance_report(controller_id).global_pnl_quote
+                for controller_id in self.controllers.keys()
+            ]
+        )
         if current_global_pnl > self.max_global_pnl:
             self.max_global_pnl = current_global_pnl
         else:
@@ -112,25 +130,32 @@ class V2WithControllers(StrategyV2Base):
                 performance_reports[controller_id] = perf
 
             # Optional debug snapshot using JSON-safe conversion
-            try:
-                snapshot = to_jsonable_python(performance_reports, fallback=str)
-                self.logger().info(f"pnldb: {json.dumps(snapshot)[:2000]}")
-            except Exception:
-                self.logger().info(f"pnldeb:Error serializing performance reports: {e}")
 
             # self._pub(performance_reports)
             self._last_performance_report_timestamp = self.current_timestamp
 
     def check_manual_kill_switch(self):
         for controller_id, controller in self.controllers.items():
-            if controller.config.manual_kill_switch and controller.status == RunnableStatus.RUNNING:
+            if (
+                controller.config.manual_kill_switch
+                and controller.status == RunnableStatus.RUNNING
+            ):
                 self.logger().info(f"Manual cash out for controller {controller_id}.")
                 controller.stop()
                 executors_to_stop = self.get_executors_by_controller(controller_id)
                 self.executor_orchestrator.execute_actions(
-                    [StopExecutorAction(executor_id=executor.id,
-                                        controller_id=executor.controller_id) for executor in executors_to_stop])
-            if not controller.config.manual_kill_switch and controller.status == RunnableStatus.TERMINATED:
+                    [
+                        StopExecutorAction(
+                            executor_id=executor.id,
+                            controller_id=executor.controller_id,
+                        )
+                        for executor in executors_to_stop
+                    ]
+                )
+            if (
+                not controller.config.manual_kill_switch
+                and controller.status == RunnableStatus.TERMINATED
+            ):
                 if controller_id in self.drawdown_exited_controllers:
                     continue
                 self.logger().info(f"Restarting controller {controller_id}.")
@@ -139,19 +164,26 @@ class V2WithControllers(StrategyV2Base):
     def check_executors_status(self):
         active_executors = self.filter_executors(
             executors=self.get_all_executors(),
-            filter_func=lambda executor: executor.status == RunnableStatus.RUNNING
+            filter_func=lambda executor: executor.status == RunnableStatus.RUNNING,
         )
         if not active_executors:
-            self.logger().info("All executors have finalized their execution. Stopping the strategy.")
+            self.logger().info(
+                "All executors have finalized their execution. Stopping the strategy."
+            )
             HummingbotApplication.main_application().stop()
         else:
             non_trading_executors = self.filter_executors(
                 executors=active_executors,
-                filter_func=lambda executor: not executor.is_trading
+                filter_func=lambda executor: not executor.is_trading,
             )
             self.executor_orchestrator.execute_actions(
-                [StopExecutorAction(executor_id=executor.id,
-                                    controller_id=executor.controller_id) for executor in non_trading_executors])
+                [
+                    StopExecutorAction(
+                        executor_id=executor.id, controller_id=executor.controller_id
+                    )
+                    for executor in non_trading_executors
+                ]
+            )
 
     def create_actions_proposal(self) -> List[CreateExecutorAction]:
         return []
@@ -167,9 +199,13 @@ class V2WithControllers(StrategyV2Base):
             if "connector_name" in config_dict:
                 if self.is_perpetual(config_dict["connector_name"]):
                     if "position_mode" in config_dict:
-                        connectors_position_mode[config_dict["connector_name"]] = config_dict["position_mode"]
+                        connectors_position_mode[config_dict["connector_name"]] = (
+                            config_dict["position_mode"]
+                        )
                     if "leverage" in config_dict:
-                        self.connectors[config_dict["connector_name"]].set_leverage(leverage=config_dict["leverage"],
-                                                                                    trading_pair=config_dict["trading_pair"])
+                        self.connectors[config_dict["connector_name"]].set_leverage(
+                            leverage=config_dict["leverage"],
+                            trading_pair=config_dict["trading_pair"],
+                        )
         for connector_name, position_mode in connectors_position_mode.items():
             self.connectors[connector_name].set_position_mode(position_mode)
